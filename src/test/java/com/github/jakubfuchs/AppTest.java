de72e7bb2199ba7357de4f04e6b9c3ba649e6661
jaf <jakub.fuchs@gmail.com>
2021-07-08 11:17:17 +0200

    target compiler setup, resources for testing git-ignored

--- src/test/java/com/github/jakubfuchs/AppTest.java
+++ src/test/java/com/github/jakubfuchs/AppTest.java
@@ -1,8 +1,13 @@
 package com.github.jakubfuchs;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 /**
  * Unit test for simple App.
@@ -16,5 +21,22 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testDecimalSeparator() {
+        DecimalFormat formatter = (DecimalFormat) DecimalFormat.getInstance(Locale.UK);
+        assertEquals('.', formatter.getDecimalFormatSymbols().getDecimalSeparator()); //true
+
+        formatter = (DecimalFormat) DecimalFormat.getInstance(Locale.GERMAN);
+        assertEquals(',', formatter.getDecimalFormatSymbols().getDecimalSeparator()); //true
+
+
+        //and in case you want another decimal separator for a specific locale
+        DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();
+        decimalFormatSymbols.setDecimalSeparator(',');
+
+        formatter.setDecimalFormatSymbols(decimalFormatSymbols);
+        assertEquals(',', formatter.getDecimalFormatSymbols().getDecimalSeparator()); //true
     }
 }
